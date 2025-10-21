102f51b4dae6deb59418005f6fcd09c577d3e153
Dmytrosat <dmytrosat@gmail.com>
2025-10-21 11:26:33 +0300

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+// WOW.js initialization
 new WOW().init();
+
+// Swiper initialization
+const swiper = new Swiper(".swiper-container", {
+    pagination: {
+        el: ".projects-pagination",
+        bulletClass: "projects-bullet",
+        bulletActiveClass: "projects-bullet-active",
+        clickable: true,
+    }, loop: true,
+});
